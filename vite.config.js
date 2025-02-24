b942db7a875379468333a416cdee3670828c3d4b
Kenny Thomas <kthomas4@ufl.edu>
2025-02-24 16:39:34 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/PersonalWebsiteV2/",
   plugins: [react()],
   optimizeDeps: {
     include: ["@react-three/drei", "three"],
